1289816fef5862d8f7a06aa9f89e4e4695b266ea
Anulekha Gopal <anulekha.k@gmail.com>
2019-11-27 11:51:28 +0530

    Lab 6

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+
 
 namespace PalTracker
 {
@@ -30,7 +32,8 @@ namespace PalTracker
                Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")
            ));
 
-           services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+        //    services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+           services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
 
            services.AddSingleton(sp => new CloudFoundryInfo(
                Configuration.GetValue<string>("PORT", "Port not configured"),
@@ -38,6 +41,9 @@ namespace PalTracker
                Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX not configured"),
                Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR not configured")
            ));
+
+           services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
